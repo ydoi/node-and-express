6553d38252add183eb92c2c4ed15acbf3f5b506f
ydoi <doi@mmmcorp.co.jp>
2018-02-10 15:29:42 +0900

    initial commit

---
+++ chapter_1/helloWorld.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { 'ContentType' : 'text/plain' });
+    res.end('Hello World!');
+}).listen(3000);
+
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
