5d86029e2e1cd6904c05d0002cd5edf1ffc51b6c
ydoi <doi@mmmcorp.co.jp>
2018-02-11 23:16:17 +0900

    [P71]use handlebars

--- projects/meadowlark/site/meadowlark.js
+++ projects/meadowlark/site/meadowlark.js
@@ -2,31 +2,33 @@ const express = require('express');
 
 const app = express();
 
+const handlebars = require('express3-handlebars')
+                   .create({ defaultLayout:'main'} );
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
-})
+    res.render('home');
+});
 
 app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-})
+    res.render('about');
+});
 
 // custom 404 page
 app.use((req, res) => {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
-})
+    res.render('404');
+});
 
 // custom 500 page
 app.use((req, res) => {
-    res.type('text/plain');
+    console.error(err.stack);
     res.status(500);
-    res.send('500 - Server Error');
-})
+    res.render('500');
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Express started on http://localhost:${app.get('port')};
