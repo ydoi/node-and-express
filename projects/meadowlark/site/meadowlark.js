840956638d6b53a8219f64ac925ae13da6950ab9
ydoi <doi@mmmcorp.co.jp>
2018-02-11 23:53:54 +0900

    [P75]add fortune

--- projects/meadowlark/site/meadowlark.js
+++ projects/meadowlark/site/meadowlark.js
@@ -9,6 +9,14 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(`${__dirname}/public`));
 
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simples",
+]
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
@@ -16,7 +24,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune});
 });
 
 // custom 404 page
